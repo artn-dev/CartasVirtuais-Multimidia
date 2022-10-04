0cab10b6305019eaac5922f7aa8513d1de06cd9c
artn-dev <azemarteixeira@gmail.com>
2022-10-04 19:05:49 -0300

    Map prefabs in the specified folder to the correct amount of markers in the scene

--- Assets/Scripts/LoadARSubstitutes.cs
+++ Assets/Scripts/LoadARSubstitutes.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using System;
 using TMPro;
 
 public class LoadARSubstitutes : MonoBehaviour
 {
-    public GameObject marker;
-
     void Start()
     {
-        GameObject virtualCard = Resources.Load("Cards/Example/IMG-00") as GameObject;
+        List<GameObject> virtualCards = GetVirtualCards();
         GameObject markersParent = GameObject.Find("Markers");
-        
+        int i = 0;
+
         foreach (Transform t in markersParent.transform.GetComponentInChildren<Transform>())
         {
-            Instantiate(virtualCard, t);
+            Instantiate(virtualCards[i], t);
+
+            if (++i >= virtualCards.Count)
+                break;
         }
+    }
+
+    private List<GameObject> GetVirtualCards()
+    {
+        var loadedObjects = Resources.LoadAll("Cards/Example", typeof(GameObject)).Cast<GameObject>();
+
+        List<GameObject> virtualCards = new List<GameObject>();
+        foreach (var loadedObject in loadedObjects)
+        {
+            virtualCards.Add(loadedObject as GameObject);
+        }
+
+        return virtualCards;
     }
 }
