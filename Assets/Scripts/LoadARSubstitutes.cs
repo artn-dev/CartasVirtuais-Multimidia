1dd673ce198f924d1e175dc2d91a2a90c3c43b2d
artn-dev <azemarteixeira@gmail.com>
2022-10-04 18:29:03 -0300

    Add virtual card to scene at runtime

--- Assets/Scripts/LoadARSubstitutes.cs
+++ Assets/Scripts/LoadARSubstitutes.cs
@@ -6,7 +6,13 @@ using TMPro;
 
 public class LoadARSubstitutes : MonoBehaviour
 {
+    public GameObject marker;
+
     void Start()
     {
+        GameObject virtualCard = Resources.Load("Cards/Example/IMG-00") as GameObject;
+        GameObject marker = GameObject.Find("clubs-king");
+
+        Instantiate(virtualCard, marker.transform);
     }
 }
